d2d9eabd1a70ea7e2908ea1075f3c073f5fc218e
Nicholas Tierney <nicholas.tierney@gmail.com>
2019-06-13 22:02:53 +1000

    added stub code for ideal gen_bib_pkg

---
+++ R/gen-bib-pkg.R
@@ -0,0 +1,19 @@
+#' @examples
+#' \dontrun{
+#' # in an ideal world:
+#' gen_bib_pkg(dplyr)
+#' #> writing to clipboard
+#' #  @Manual{dplyr,
+#' #  title = {dplyr: A Grammar of Data Manipulation},
+#' #  author = {Hadley Wickham and Romain François and Lionel Henry and Kirill Müller},
+#' #  year = {2019},
+#' #  note = {R package version 0.8.1},
+#' #  url = {https://CRAN.R-project.org/package=dplyr},
+#' #  }
+#' # }
+gen_bib_pkg <- function(pkg,
+                        handle = pkg,
+                        clipboard = FALSE){
+
+}
+
