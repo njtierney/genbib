44372d9f145a6e741f55c489dcc53da845495b5e
Michael DeWitt <me.dewitt.jr@gmail.com>
2019-12-28 15:45:51 -0500

    updates to some of the text

--- R/gen-bib-addin.R
+++ R/gen-bib-addin.R
@@ -1,11 +1,13 @@
 #' Make it easier to cite web addresses
+#' Based on 'Twitter Rant!' Addin UI from Bob Rudis; see
+#'    \url{https://github.com/hrbrmstr/hrbraddins}
 #'
 #' @export
 #'
 
 gen_bib_addin <- function(){
   ui <- miniPage(
-    gadgetTitleBar("Generate a Website BibTex Entry!"),
+    gadgetTitleBar("Generate a Website BibTeX Entry!"),
     miniContentPanel(
       textAreaInput("inHandle", "Handle for Website", cols = 120, rows = 5),
       textAreaInput("inUrl", "Website URL", cols = 120, rows = 5),
@@ -35,7 +37,7 @@ gen_bib_addin <- function(){
     })
   }
 
-  # Use a modal dialog as a viewr.
-  viewer <- dialogViewer("Twitter Rant!", width = 1000, height = 800)
+  # Use a modal dialog as a viewer
+  viewer <- dialogViewer("BibTeX on the Fly!", width = 1000, height = 800)
   runGadget(ui, server, viewer = viewer)
 }
